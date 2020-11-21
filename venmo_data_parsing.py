ac3f4c45ceb8d0f912392b4184a8d0b44d2ed0fa
Max Burdett <ubercharge60@gmail.com>
2020-11-21 16:55:39 -0500

    Implemented Create_payments() to return a list of each payment as an object

--- venmo_data_parsing.py
+++ venmo_data_parsing.py
@@ -22,18 +22,32 @@ class Person:
     def add_to_recieved(self, ammount):
         self.__recieved += ammount
 
+    def get_name(self):
+        return self.__name
+    def get_paid(self):
+        return self.__paid
+    def get_recieved(self):
+        return self.__recieved
+
 
 class Payment:
     __slots__ = ['__datetime', '__type', '__from', '__to', '__ammount', '__fee', '__funding_source', '__note']
     def __init__(self, datetime, ty, note, fro, to, ammount, fee, funding_source):
+        if fee == '':
+            fee = 0.0
+        if ammount == '':
+            ammount = 0.0
+        
+        
         self.__datetime = datetime
         self.__type = ty
         self.__from = fro
         self.__to = to
         self.__ammount = ammount
-        self.__fee = fee
         self.__funding_source = funding_source
         self.__note = note
+        self.__fee = fee
+
 
     def __str__(self):
         final = ""
@@ -41,7 +55,7 @@ class Payment:
         final += "Type: " + self.__type
         final += "\nFrom: " + self.__from
         final += "\nTo: " + self.__to
-        final += "\nAmmount: " + self.__ammount
+        final += "\nAmmount: " + str(self.__ammount)
         final += "\nNote: " + self.__note
 
         return final
@@ -70,23 +84,39 @@ def create_payments(filename):
     '''
     payments_list = []
 
-    with open(filename) as file:
-        csv_read = csv.reader(file)
+    with open(filename, encoding='utf-8') as csv_file:
+        csv_read = csv.reader(csv_file)
         next(csv_read)
         next(csv_read)
 
+
         for record in csv_read:
-            temp_payment = Payment(record[2], record[3], record[5], record[6], record[7], float(record[8][2:]), float(record[9][2:]))
+            temp_payment = Payment(record[2], record[3], record[5], record[6], record[7], record[8][3:], record[9][3:], record[11])
             payments_list.append(temp_payment)
 
     return payments_list
 
+def create_people(payments_list):
+    people_list = []
+    for payment in payments_list:
+        if payment.get_type() == "Payment":
+            pass
 
+def merge_people(person1, person2):
+    if person1.get_name == person2.get_name:#if its the same person then we can merge it
+        temp_person = Person(person1.get_name(), person1.get_paid() + person2.get_paid(), person1.get_recieved() + person2.get_recieved())
 
+    return temp_person
+
+'''
+
+THIS NEEDS TO BE REPLACED BY merge_people()
+
+LEGACY SHIZZ
 def concat_lists(li1, li2, li3=None, li4=None, li5=None, li6=None):
-    '''
+    
     function will concatonate up to 6 lists at once, requires at least 2 as input
-    '''
+    
     final = []
 
     final.append(li1)
@@ -103,6 +133,8 @@ def concat_lists(li1, li2, li3=None, li4=None, li5=None, li6=None):
 
     return final
 
+'''
+
 def sum_list(li):
     sum = 0
     for item in li:
@@ -110,10 +142,12 @@ def sum_list(li):
 
     return sum
 
+
 def main():
     test = create_payments('Venmo Statements/oct_statement_2020.csv')
 
     for item in test:
+        print('----------')
         print(item)
 
 
