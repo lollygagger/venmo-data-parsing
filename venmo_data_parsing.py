8ec3fc7c59ba46b77a76a4f52cd5136978186ba0
Max Burdett <ubercharge60@gmail.com>
2020-11-21 17:14:45 -0500

    added comments for readability and cleaned up code

--- venmo_data_parsing.py
+++ venmo_data_parsing.py
@@ -4,7 +4,7 @@ class Person:
     '''
     data class for a singular person on venmo
 
-    class constructor takes in an ammount paid and an ammount recieved
+    class constructor takes in an ammount paid and an ammount recieved and a name
 
 
     '''
@@ -31,6 +31,11 @@ class Person:
 
 
 class Payment:
+    '''
+    The payment class is a data class meant to store the information from one transaction on venmo
+
+    the constructor takes in (about) all the data from one line of a venmo statement csv file
+    '''
     __slots__ = ['__datetime', '__type', '__from', '__to', '__ammount', '__fee', '__funding_source', '__note']
     def __init__(self, datetime, ty, note, fro, to, ammount, fee, funding_source):
         if fee == '':
@@ -59,6 +64,21 @@ class Payment:
         final += "\nNote: " + self.__note
 
         return final
+
+    def __repr__(self):
+        final = ""
+
+        final += "Datetime: " + self.__datetime
+        final += "\nType: " + self.__type
+        final += "\nFrom: " + self.__from
+        final += "\nTo: " + self.__to
+        final += "\nAmmount: " + str(self.__ammount)
+        final += "\nFee: " + str(self.__fee)
+        final += "\nFunding Source: " + self.__funding_source
+        final += "\nNote: " + self.__note
+
+        return final
+
     
     def get_datetime(self):
         return self.__datetime
@@ -97,12 +117,23 @@ def create_payments(filename):
     return payments_list
 
 def create_people(payments_list):
+    '''
+    function will take a list of payment objects then parse through them to 
+    take data on each person who has send or recieved a venmo
+
+    function will return a list of people objects which each contain
+    the information for one person interacting with venmo
+    '''
     people_list = []
     for payment in payments_list:
         if payment.get_type() == "Payment":
             pass
 
 def merge_people(person1, person2):
+    '''
+    function takes in 2 person objects and if they share the same name then it 
+    will merge the 2 into another person which it returns
+    '''
     if person1.get_name == person2.get_name:#if its the same person then we can merge it
         temp_person = Person(person1.get_name(), person1.get_paid() + person2.get_paid(), person1.get_recieved() + person2.get_recieved())
 
@@ -136,6 +167,7 @@ def concat_lists(li1, li2, li3=None, li4=None, li5=None, li6=None):
 '''
 
 def sum_list(li):
+    #helper function to return the sum of all items in a list(as long as they are ints)
     sum = 0
     for item in li:
         sum += int(item)
@@ -148,7 +180,7 @@ def main():
 
     for item in test:
         print('----------')
-        print(item)
+        print(repr(item))
 
 
 if __name__ == "__main__":
